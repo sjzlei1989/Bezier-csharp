9ee73a350bd122b5c533e0dbe084b7dec09c887d
xiaolei <sjzlei1989@gmail.com>
2018-06-15 13:46:25 +0800

    添加项目文件。

---
+++ Bezier/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Bezier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictrueBox = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.trackBarValueLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictrueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictrueBox
+            // 
+            this.pictrueBox.Location = new System.Drawing.Point(0, 0);
+            this.pictrueBox.Margin = new System.Windows.Forms.Padding(0);
+            this.pictrueBox.Name = "pictrueBox";
+            this.pictrueBox.Size = new System.Drawing.Size(785, 563);
+            this.pictrueBox.TabIndex = 0;
+            this.pictrueBox.TabStop = false;
+            this.pictrueBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar.Location = new System.Drawing.Point(314, 13);
+            this.trackBar.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar.Minimum = 3;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(100, 45);
+            this.trackBar.TabIndex = 1;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar.Value = 3;
+            this.trackBar.ValueChanged += new System.EventHandler(this.trackBar_valueChanged);
+            // 
+            // trackBarValueLabel
+            // 
+            this.trackBarValueLabel.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.trackBarValueLabel.Location = new System.Drawing.Point(417, 13);
+            this.trackBarValueLabel.Name = "trackBarValueLabel";
+            this.trackBarValueLabel.Size = new System.Drawing.Size(29, 45);
+            this.trackBarValueLabel.TabIndex = 2;
+            this.trackBarValueLabel.Text = "10";
+            this.trackBarValueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(179, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "控制点的数量";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarValueLabel);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictrueBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictrueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictrueBox;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label trackBarValueLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Bezier/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bezier
+{
+    public partial class Form1 : Form {
+        const int DOTWIDTH = 10;
+        Graphics g;
+        List<PointF> dots = new List<PointF>();
+        int _controlPointsCount = 0;
+        int controlPointsCount {
+            set {
+                _controlPointsCount = value;
+                trackBarValueLabel.Text = _controlPointsCount.ToString();
+            }
+            get {
+                return _controlPointsCount;
+            }
+        }
+
+        /// <summary>
+        /// 是否已经画过线了
+        /// </summary>
+        bool drawed = false;
+        public Form1() {
+            InitializeComponent();
+            controlPointsCount = trackBar.Value;
+            Image img = new Bitmap(pictrueBox.Width, pictrueBox.Height);
+            pictrueBox.Image = img;
+            g = Graphics.FromImage(img);
+            g.Clear(Color.White);
+        }
+
+        private void trackBar_valueChanged(object sender, EventArgs e) {
+            controlPointsCount = trackBar.Value;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e) {
+            if(drawed) {
+                drawed = false;
+                dots.Clear();
+                g.Clear(Color.White);
+                pictrueBox.Refresh();
+                return;
+            }
+            if(dots.Count < controlPointsCount) {
+                MouseEventArgs margs = e as MouseEventArgs;
+                DrawDot(g, Brushes.Red, margs.X, margs.Y, DOTWIDTH);
+                pictrueBox.Refresh();
+                string str = string.Format("[{0}, {1}]", margs.X, margs.Y);
+                g.DrawString(str, new Font("宋体", 10), Brushes.Green, 10, 10 + dots.Count * 20);
+                pictrueBox.Refresh();
+                dots.Add(margs.Location);
+                if(dots.Count == controlPointsCount) {
+                    //TODO 开始画线
+                    List<Segment> segs = new List<Segment>();
+                    for(int i = 0; i < dots.Count - 1; i++) {
+                        g.DrawLine(Pens.Gray, dots[i], dots[i + 1]);
+                        segs.Add(new Segment(dots[i], dots[i + 1]));
+                    }
+                    float radio = 0.0f; //比例
+                    float step = 0.01f; //步进值
+                    while(radio <= 1.0f) {
+                        DrawDot(g, Brushes.Red, FindBezierPoint(dots.ToArray(), radio), DOTWIDTH);
+                        pictrueBox.Refresh();
+                        radio += step;
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    drawed = true;
+                }
+            }
+        }
+
+        private PointF FindBezierPoint(PointF[] _points, float _r) {
+            var segs = GetSegments(_points);
+            if(segs == null) {
+                throw new Exception("no segments");
+            }
+            if(segs.Length == 1) {
+                return segs[0].GetPointByDistance(_r);
+            }
+            else {
+                List<PointF> tmpPoints = new List<PointF>();
+                for(int i = 0; i < segs.Length; i++) {
+                    tmpPoints.Add(segs[i].GetPointByDistance(_r));
+                }
+                return FindBezierPoint(tmpPoints.ToArray(), _r);
+            }
+        }
+
+        private Segment[] GetSegments(PointF[] _points) {
+            if(_points.Length <= 1) {
+                return null;
+            }
+            List<Segment> segs = new List<Segment>();
+            for(int i = 0; i < _points.Length - 1; i++) {
+                Segment s = new Segment(_points[i], _points[i + 1]);
+                segs.Add(s);
+            }
+            return segs.ToArray();
+        }
+
+        private void DrawDot(Graphics _g, Brush _b, PointF _p, float _width) {
+            DrawDot(_g, _b, _p.X, _p.Y, _width);
+        }
+        private void DrawDot(Graphics _g, Brush _b, float _x, float _y, float _width) {
+            _g.FillEllipse(_b, new RectangleF(_x - _width / 2, _y - _width / 2, _width, _width));
+        }
+        private void DrawSegment(Graphics _g, Pen _p, Segment _s) {
+            _g.DrawLine(_p, _s.start, _s.end);
+        }
+    }
+
+    public class Segment
+    {
+        public PointF start;
+        public PointF end;
+
+        public Segment(PointF _s, PointF _e) {
+            start = _s;
+            end = _e;
+        }
+
+        /// <summary>
+        /// 获取距离起点一定距离的点的坐标
+        /// </summary>
+        /// <param name="_d"></param>
+        /// <returns></returns>
+        public PointF GetPointByDistance(float _d) {
+            if(_d < 0 || _d > 1) {
+                return Point.Empty;
+            }
+            return new PointF(start.X + (end.X - start.X) * _d, start.Y + (end.Y - start.Y) * _d);
+        }
+    }
+}
